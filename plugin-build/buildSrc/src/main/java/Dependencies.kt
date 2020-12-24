8ebe60808ec444d0ee9d53d113a43bb11cfcca6a
Nicola Corti <corti.nico@gmail.com>
2020-12-24 12:49:32 +0100

    Bump Ktfmt to 0.19 (#1)

--- plugin-build/buildSrc/src/main/java/Dependencies.kt
+++ plugin-build/buildSrc/src/main/java/Dependencies.kt
@@ -1,17 +1,17 @@
 object Versions {
     const val AGP = "4.1.1"
-    const val COROUTINES = "1.3.9"
+    const val COROUTINES = "1.4.2"
     const val DIFF_UTIL = "4.9"
     const val JUPITER = "5.7.0"
     const val KTLINT = "0.39.0"
-    const val KTFMT = "0.18"
+    const val KTFMT = "0.19"
     const val TRUTH = "1.1"
 }
 
 object BuildPluginsVersion {
     const val BINARY_COMPATIBILITY_VALIDATOR = "0.2.4"
     const val DETEKT = "1.15.0"
-    const val KOTLIN = "1.3.72"
+    const val KOTLIN = "1.4.21"
     const val KTLINT = "9.4.1"
     const val PLUGIN_PUBLISH = "0.12.0"
     const val VERSIONS_PLUGIN = "0.33.0"
--- plugin-build/plugin/src/test/resources/jvmProject/build.gradle.kts
+++ plugin-build/plugin/src/test/resources/jvmProject/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.21"
     id("com.ncorti.ktfmt.gradle")
 }
 
