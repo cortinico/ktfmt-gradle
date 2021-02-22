275e06bf73bab930b6a57c9e1d0d9cbdd209858e
Nicola Corti <corti.nico@gmail.com>
2021-02-22 09:53:48 +0100

    Bump multiple dependencies (#7)
    * Bump multiple dependencies
    
    * Bump more dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,13 +1,11 @@
 object Versions {
-    const val KTLINT = "0.39.0"
-    const val JUPITER = "5.7.0"
+    const val JUPITER = "5.7.1"
 }
 
 object BuildPluginsVersion {
     const val DETEKT = "1.15.0"
     const val KOTLIN = "1.4.21"
-    const val KTLINT = "9.4.1"
-    const val VERSIONS_PLUGIN = "0.33.0"
+    const val VERSIONS_PLUGIN = "0.36.0"
 }
 
 object TestingLib {
