f6179065a43068ee346b8493e4c7d344a80aca3e
Nicola Corti <corti.nico@gmail.com>
2021-04-09 09:44:39 +0200

    Remove resolutionStrategy block

--- plugin-build/settings.gradle.kts
+++ plugin-build/settings.gradle.kts
@@ -1,12 +1,4 @@
 pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "binary-compatibility-validator") {
-                useModule("org.jetbrains.kotlinx:binary-compatibility-validator:${requested.version}")
-            }
-        }
-    }
-
     repositories {
         gradlePluginPortal()
         mavenCentral()
