cc365429190598fa6a54d7d298fd9716c5dbff9b
Nicola Corti <corti.nico@gmail.com>
2021-11-20 15:07:12 +0000

    KtFmt to 0.30

--- plugin-build/buildSrc/src/main/java/Dependencies.kt
+++ plugin-build/buildSrc/src/main/java/Dependencies.kt
@@ -3,7 +3,7 @@ object Versions {
     const val COROUTINES = "1.5.1"
     const val DIFF_UTIL = "4.9"
     const val JUPITER = "5.7.2"
-    const val KTFMT = "0.28"
+    const val KTFMT = "0.30"
     const val TRUTH = "1.1.3"
 }
 
