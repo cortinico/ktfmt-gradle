8b036252fcc455aad974a8ccf0eb64f685808d2f
Nicola Corti <corti.nico@gmail.com>
2023-07-15 12:46:17 +0100

    Fix Gradle 9 warnings (#165)

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -10,6 +10,7 @@ ktfmt {
 dependencies {
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.jupiter)
+    testRuntimeOnly(libs.jupiter.platform.launcher)
 }
 
 tasks.withType<Test> {
