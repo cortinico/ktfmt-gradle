ea2deece0670738a7b0fae6ce04cd51e97f5b14c
gpopides <gpopides@pm.me>
2024-03-22 21:47:49 +0100

    Remove input directory from task

--- plugin-build/plugin/src/main/java/com/ncorti/ktfmt/gradle/tasks/CreateGitPreCommitHookTask.kt
+++ plugin-build/plugin/src/main/java/com/ncorti/ktfmt/gradle/tasks/CreateGitPreCommitHookTask.kt
@@ -2,29 +2,19 @@ package com.ncorti.ktfmt.gradle.tasks
 
 import com.ncorti.ktfmt.gradle.tasks.worker.PreCommitHookFormatAction
 import javax.inject.Inject
-import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.PathSensitive
-import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
-import org.gradle.work.Incremental
 import org.gradle.workers.WorkerExecutor
 
 /** ktfmt-gradle Git hook task. Creates a git hook that runs ktfmt on git commit */
 abstract class CreateGitPreCommitHookTask : SourceTask() {
     @Inject abstract fun getWorkerExecutor(): WorkerExecutor
 
-    @get:InputDirectory
-    @get:Incremental
-    @get:PathSensitive(PathSensitivity.RELATIVE)
-    abstract val projectDir: DirectoryProperty
-
     @TaskAction
     fun createHook() {
         val workQueue = getWorkerExecutor().noIsolation()
         workQueue.submit(PreCommitHookFormatAction::class.java) { parameters ->
-            parameters.projectDir.set(projectDir)
+            parameters.projectDir.set(project.projectDir)
         }
     }
 }
