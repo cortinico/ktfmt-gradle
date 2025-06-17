063f1d9f3aca406e1f4c9431091e755f13df7ed6
Nicola Corti <corti.nico@gmail.com>
2025-06-17 11:29:35 +0100

    Bump examples to JDK 17

--- plugin-build/plugin/build.gradle.kts
+++ plugin-build/plugin/build.gradle.kts
@@ -9,15 +9,15 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 kotlin {
     compilerOptions {
         apiVersion.set(KotlinVersion.fromVersion("1.6"))
         languageVersion.set(KotlinVersion.fromVersion("1.6"))
-        jvmTarget = JvmTarget.JVM_11
+        jvmTarget = JvmTarget.JVM_17
     }
 }
 
