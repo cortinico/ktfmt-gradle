2519f96549fa5cd727ce6a0ebf302712a4870854
Sam Doward <sam.doward@gmail.com>
2026-03-09 10:20:51 +0100

    Update AGP 8.13.0 -> 9.1.0

--- plugin-build/plugin/src/main/java/com/ncorti/ktfmt/gradle/KtfmtAndroidUtils.kt
+++ plugin-build/plugin/src/main/java/com/ncorti/ktfmt/gradle/KtfmtAndroidUtils.kt
@@ -1,9 +1,10 @@
 package com.ncorti.ktfmt.gradle
 
-import com.android.build.gradle.BaseExtension
-import com.android.build.gradle.internal.api.DefaultAndroidSourceDirectorySet
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.DynamicFeatureExtension
+import com.android.build.api.dsl.LibraryExtension
+import com.android.build.api.dsl.TestExtension
 import com.ncorti.ktfmt.gradle.KtfmtPluginUtils.createTasksForSourceSet
-import java.util.concurrent.Callable
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.tasks.TaskProvider
@@ -17,44 +18,60 @@ internal object KtfmtAndroidUtils {
         ktfmtExtension: KtfmtExtension,
         isKmpProject: Boolean = false,
     ) {
-        fun applyKtfmtForAndroid() {
-            project.extensions.configure(BaseExtension::class.java) {
-                it.sourceSets.configureEach { sourceSet ->
-                    @Suppress("DEPRECATION")
-                    val sourceSetName =
-                        if (isKmpProject) {
-                            "kmp ${sourceSet.name}"
-                        } else {
-                            sourceSet.name
-                        }
-                    val srcDirs =
-                        sourceSet.java.srcDirs +
-                            runCatching {
-                                    // As sourceSet.kotlin doesn't exist before AGP 7
-                                    (sourceSet.kotlin as? DefaultAndroidSourceDirectorySet)?.srcDirs
-                                }
-                                .getOrNull()
-                                .orEmpty()
-                    // Passing Callable, so returned FileCollection, will lazy evaluate it
-                    // only when task will need it.
-                    // Solves the problem of having additional source dirs in
-                    // current AndroidSourceSet, that are not available on eager
-                    // evaluation.
-                    createTasksForSourceSet(
-                        project,
-                        sourceSetName,
-                        project.files(Callable { srcDirs }),
-                        ktfmtExtension,
-                        topLevelFormat,
-                        topLevelCheck,
-                    )
-                }
-            }
+        val marker = "ktfmt.android.tasks.created"
+        if (project.extensions.extraProperties.has(marker)) {
+            return
         }
 
-        project.plugins.withId("com.android.application") { applyKtfmtForAndroid() }
-        project.plugins.withId("com.android.library") { applyKtfmtForAndroid() }
-        project.plugins.withId("com.android.test") { applyKtfmtForAndroid() }
-        project.plugins.withId("com.android.dynamic-feature") { applyKtfmtForAndroid() }
+        val androidExtension = project.extensions.findByName("android") ?: return
+        project.extensions.extraProperties.set(marker, true)
+
+        fun createAndroidSourceSetTasks(name: String, srcDirs: Set<String>) {
+            val sourceSetName = if (isKmpProject) "kmp $name" else name
+            createTasksForSourceSet(
+                project,
+                sourceSetName,
+                project.files(*srcDirs.toTypedArray()),
+                ktfmtExtension,
+                topLevelFormat,
+                topLevelCheck,
+            )
+        }
+
+        when (androidExtension) {
+            is ApplicationExtension ->
+                androidExtension.sourceSets.configureEach {
+                    sourceSet: com.android.build.api.dsl.AndroidSourceSet ->
+                    createAndroidSourceSetTasks(
+                        sourceSet.name,
+                        sourceSet.kotlin.directories + sourceSet.java.directories,
+                    )
+                }
+            is LibraryExtension ->
+                androidExtension.sourceSets.configureEach {
+                    sourceSet: com.android.build.api.dsl.AndroidSourceSet ->
+                    createAndroidSourceSetTasks(
+                        sourceSet.name,
+                        sourceSet.kotlin.directories + sourceSet.java.directories,
+                    )
+                }
+            is TestExtension ->
+                androidExtension.sourceSets.configureEach {
+                    sourceSet: com.android.build.api.dsl.AndroidSourceSet ->
+                    createAndroidSourceSetTasks(
+                        sourceSet.name,
+                        sourceSet.kotlin.directories + sourceSet.java.directories,
+                    )
+                }
+            is DynamicFeatureExtension ->
+                androidExtension.sourceSets.configureEach {
+                    sourceSet: com.android.build.api.dsl.AndroidSourceSet ->
+                    createAndroidSourceSetTasks(
+                        sourceSet.name,
+                        sourceSet.kotlin.directories + sourceSet.java.directories,
+                    )
+                }
+            else -> Unit
+        }
     }
 }
