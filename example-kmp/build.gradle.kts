9b8ea299a786e4a609df44c27d846fd6f697739a
Sam Doward <sam.doward@gmail.com>
2026-03-09 10:53:19 +0100

    Improve example-kmp structure
    - Remove android config
    - Change source sets

--- example-kmp/build.gradle.kts
+++ example-kmp/build.gradle.kts
@@ -1,25 +1,13 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
     id("com.ncorti.ktfmt.gradle")
 }
 
 kotlin {
     jvmToolchain(17)
     jvm()
-    androidTarget()
 }
 
 ktfmt { kotlinLangStyle() }
 
-android {
-    namespace = "com.ncorti.example.ktfmt"
-    compileSdk = 33
-}
-
 tasks.withType<Test> { useJUnitPlatform() }
-
-dependencies {
-    testImplementation(platform(libs.junit.bom))
-    testImplementation(libs.jupiter)
-}
