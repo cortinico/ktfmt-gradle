ca1807a6c22ce7e7789118906d1670864752b3f8
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-06-30 16:35:52 +0200

    Fixes based on feedback

--- example-kmp/build.gradle.kts
+++ example-kmp/build.gradle.kts
@@ -1,13 +1,30 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kmp.library)
     id("com.ncorti.ktfmt.gradle")
 }
 
 kotlin {
     jvmToolchain(17)
     jvm()
-}
 
-ktfmt { kotlinLangStyle() }
+    ktfmt { kotlinLangStyle() }
+
+    android {
+        namespace = "com.ncorti.example.ktfmt"
+        compileSdk = 33
+        withHostTest {}
+    }
+
+    sourceSets {
+        jvmTest {
+            dependencies {
+                implementation(project.dependencies.platform(libs.junit.bom))
+                implementation(libs.jupiter)
+                runtimeOnly(libs.jupiter.platform.launcher)
+            }
+        }
+    }
+}
 
 tasks.withType<Test> { useJUnitPlatform() }
--- plugin-build/plugin/src/main/java/com/ncorti/ktfmt/gradle/KtfmtAndroidUtils.kt
+++ plugin-build/plugin/src/main/java/com/ncorti/ktfmt/gradle/KtfmtAndroidUtils.kt
@@ -1,10 +1,8 @@
 package com.ncorti.ktfmt.gradle
 
-import com.android.build.api.dsl.ApplicationExtension
-import com.android.build.api.dsl.DynamicFeatureExtension
-import com.android.build.api.dsl.LibraryExtension
-import com.android.build.api.dsl.TestExtension
+import com.android.build.api.dsl.CommonExtension
 import com.ncorti.ktfmt.gradle.KtfmtPluginUtils.createTasksForSourceSet
+import java.util.concurrent.Callable
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.tasks.TaskProvider
@@ -19,9 +17,6 @@ internal object KtfmtAndroidUtils {
         isKmpProject: Boolean = false,
     ) {
         val marker = "ktfmt.android.tasks.created"
-        if (project.extensions.extraProperties.has(marker)) {
-            return
-        }
 
         val androidExtension = project.extensions.findByName("android") ?: return
         project.extensions.extraProperties.set(marker, true)
@@ -31,47 +26,19 @@ internal object KtfmtAndroidUtils {
             createTasksForSourceSet(
                 project,
                 sourceSetName,
-                project.files(srcDirs),
+                project.files(Callable { srcDirs }),
                 ktfmtExtension,
                 topLevelFormat,
                 topLevelCheck,
             )
         }
 
-        when (androidExtension) {
-            is ApplicationExtension ->
-                androidExtension.sourceSets.configureEach {
-                    sourceSet: com.android.build.api.dsl.AndroidSourceSet ->
-                    createAndroidSourceSetTasks(
-                        sourceSet.name,
-                        sourceSet.kotlin.directories + sourceSet.java.directories,
-                    )
-                }
-            is LibraryExtension ->
-                androidExtension.sourceSets.configureEach {
-                    sourceSet: com.android.build.api.dsl.AndroidSourceSet ->
-                    createAndroidSourceSetTasks(
-                        sourceSet.name,
-                        sourceSet.kotlin.directories + sourceSet.java.directories,
-                    )
-                }
-            is TestExtension ->
-                androidExtension.sourceSets.configureEach {
-                    sourceSet: com.android.build.api.dsl.AndroidSourceSet ->
-                    createAndroidSourceSetTasks(
-                        sourceSet.name,
-                        sourceSet.kotlin.directories + sourceSet.java.directories,
-                    )
-                }
-            is DynamicFeatureExtension ->
-                androidExtension.sourceSets.configureEach {
-                    sourceSet: com.android.build.api.dsl.AndroidSourceSet ->
-                    createAndroidSourceSetTasks(
-                        sourceSet.name,
-                        sourceSet.kotlin.directories + sourceSet.java.directories,
-                    )
-                }
-            else -> Unit
+        val ext = androidExtension as? CommonExtension ?: return
+        ext.sourceSets.configureEach { sourceSet ->
+            createAndroidSourceSetTasks(
+                sourceSet.name,
+                sourceSet.kotlin.directories + sourceSet.java.directories,
+            )
         }
     }
 }
